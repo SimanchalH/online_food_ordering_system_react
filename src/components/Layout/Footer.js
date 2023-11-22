9056db72703f87972bb39d501811606ef30b4cc8
SimanchalH <118511775+SimanchalH@users.noreply.github.com>
2023-11-22 14:48:34 +0530

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,9 +1,8 @@
 import React,{Fragment} from 'react'
 import { Link} from 'react-router-dom'
 import { connect } from 'react-redux';
-import { PropTypes } from 'prop-types';
-//import { logout } from '../../actions/auth';
-import { LOGOUT } from '../../actions/types';
+
+
 
 
 const Landing = ({isAuthenticated}) => {
