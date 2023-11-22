bd88f6f5e88d66f90332d23eaae12c5c13fc64a8
SimanchalH <118511775+SimanchalH@users.noreply.github.com>
2023-11-22 15:22:47 +0530

    Update OrderDetails.js

--- src/components/Order/OrderDetails.js
+++ src/components/Order/OrderDetails.js
@@ -3,10 +3,8 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Parser from 'html-react-parser';
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
-import { Link } from 'react-router-dom';
 import config from '../../utils/config';
-import { withRouter } from "react-router"
-import { useSearchParams, useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const OrderDetails = () => {
 
